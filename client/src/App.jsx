cdd532c18b0163171dbd58a99b8b96a79b455d17
b-wang43 <wang.blair210@gmail.com>
2023-10-09 20:12:34 -0400

    Created Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import LogIn from './LogIn'
+import SignUp from './SignUp'
+import Profile from '../Profile'
+import About from './About'
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<LogIn />}/>
+      <Route path='/sign-up' element={<SignUp />}/>
+      <Route path='/profile' element={<Profile />}/>
+      <Route path='/about' element={<About />}/>
+    </Routes>
+  </BrowserRouter>
 }
