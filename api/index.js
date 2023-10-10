4d4dd838eb70315c1759042149ef52dd5415ae74
b-wang43 <wang.blair210@gmail.com>
2023-10-09 21:37:35 -0400

    Connected to database (MongoDB)

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.KEY)
+.then(() => {
+    console.log('Connected to database');
+})
+.catch((err) => {
+    console.log(err);
+});
+
 const app = express();
 app.listen(5173, () => {
     console.log('Server is running on port 5173');
